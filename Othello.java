c320f9b619d01ed9637068198b8c140a9f522a8c
Rebecca <rebeccaplease@gmail.com>
2015-10-16 12:48:10 -0400

    fixed bottom check of validSearch. deleted test files from github

--- Othello.java
+++ Othello.java
@@ -9,42 +9,148 @@ public class Othello{
    static char blank = 0;
 
    public static void main(String[] args) throws FileNotFoundException{
-      Scanner sc = new Scanner(new File("test2.txt"));
+      // Scanner sc = new Scanner(new File("test.txt"));
    
-      int r = 0; 
-      int c = 0;
-      while(sc.hasNext()){
-         if(c > size-1){
-            r++;
-            c = 0;
+      // int r = 0; 
+      // int c = 0;
+      // while(sc.hasNext()){
+      //    if(c > size-1){
+      //       r++;
+      //       c = 0;
+      //    }
+      //    board[r][c] = sc.nextInt();
+      //    c++;
+      // }
+      // sc.close();
+
+      //fill up board
+
+      
+      for(int r = 0; r < size; r++){
+         for(int c = 0; c < size; c++){
+            board[r][c] = blank;
          }
-         board[r][c] = sc.nextInt();
-         c++;
       }
-      sc.close();
-      char one = 1;
-      char two = 2;
+
+      board[3][3] = 1;
+      board[3][4] = 2;
+      board[4][3] = 2;
+      board[4][4] = 1;
+
       printBoard();
-      validMove(two, one);
-      //print out legal moves
-      for(Move object: legalMoves){
-         System.out.println(object.toString());
-      }
+      //player,enemy
+      validMove(2,1);
+      printLegalMoves();
+      // char one = 1;
+      // char two = 2;
+      // printBoard();
+      // validMove(one, two);
+
+      //init();
       
    }
 
-   public static void printBoard(){
+   public static void testBoar
+
+   public static void init(){
       for(int r = 0; r < size; r++){
          for(int c = 0; c < size; c++){
-            System.out.print(board[r][c] + " ");
+            board[r][c] = blank;
          }
-         System.out.println();
       }
+
+      board[3][3] = 1;
+      board[3][4] = 2;
+      board[4][3] = 2;
+      board[4][4] = 1;
+      
+
+      Scanner in = new Scanner(System.in);
+      System.out.println("Do you want to go first[1] or second[2]?");//need to catch invalid inputs
+      int p = in.nextInt();
+      char comp;
+      Player player = new Player(p,false);
+      Player computer;
+
+      if(p == 1){
+         comp = 2;
+         computer = new Player(comp,true);
+      }
+      else{
+         comp = 1;
+         computer = new Player(comp,true);
+      }
+
+      //while(!gameOver()){
+      while(true){
+         if(p == 1){
+            play(player, comp, in);
+            //stateCheck();
+            play(computer, p, in);
+            //stateCheck();
+         }
+         else {
+            play(computer, p, in);
+            //stateCheck();
+            play(player, comp, in);
+            //stateCheck(); 
+         }
+      }
+   }
+
+   public static void printBoard(){
+      //save string?
+      System.out.println("    0   1   2   3   4   5   6   7");
+      System.out.println("   ================================");
+      for(int r = 0; r < size; r++){
+         System.out.print(r + " |");
+         for(int c = 0; c < size; c++){
+            if(board[r][c] == blank)
+               System.out.print("   |");
+            else
+               System.out.print(" " + board[r][c] + " |");
+         }
+         System.out.println("\n   --------------------------------");
+      }
+      //System.out.println("   ================================");
+   }
+   public static void printLegalMoves(){
+      System.out.println("printing legal moves " + legalMoves.size());
+      int index = 0;
+      for(Move object: legalMoves){
+         System.out.println("["+ index + "] " + object.toString());
+         index++;
+      }
+   }
+
+   public static void play(Player current, int enemy, Scanner in){
+      validMove(current.symbol, enemy); //fill arraylist with possible moves
+      if(legalMoves.size() == 0){ //if there are no valid moves
+
+      }
+      printBoard();
+      printLegalMoves();
+      Move chosen;
+      if(!current.ai){ //if player is human 
+         //need to check for valid input
+         // boolean entered = false;
+         
+         System.out.println("Pick a move!");
+
+         chosen = legalMoves.get(in.nextInt());
+      }
+      else{ //if computer
+         int random = (int)(Math.random()*legalMoves.size());
+         chosen = legalMoves.get(random);
+      }
+      board[chosen.row][chosen.col] = current.symbol;
+      printBoard();
    }
 
 	//find all legal moves and add to the legalMoves ArrayList
    public static void validMove(int player, int enemy){
    	//loop through board 
+      legalMoves.clear(); //clear list
       for(int r = 0; r < size; r++){
          for(int c = 0; c < size; c++){
             if(board[r][c] == blank){
@@ -147,7 +253,7 @@ public class Othello{
          return false;
       }
       else if(dir.equals("down")){
-         while(r > size-1){  
+         while(r < size-1){  
             r++;
             if (board[r][c] == blank)
                return false;
@@ -247,7 +353,15 @@ public class Othello{
          col = c;
       }
       public String toString(){
-         return "["+row + "," + col + "]";
+         return "("+ (row) + "," + (col) + ")";
+      }
+   }
+   public static class Player{
+      int symbol;
+      boolean ai;
+      public Player(int s, boolean comp){
+         symbol = s;
+         ai = comp;
       }
    }
 }
