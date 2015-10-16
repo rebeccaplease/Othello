8a4f72beb1f26f1847d964da0b7828019a73065d
Rebecca <rebeccaplease@gmail.com>
2015-10-16 19:40:35 -0400

    flipping pieces works

--- Othello.java
+++ Othello.java
@@ -133,22 +133,74 @@ public class Othello{
    public static void flip(int r, int c, int player, int enemy){
       int row = r;
       int col = c;
-      if(validSearch("left", r, c, player, enemy))
-         board[r-1][c] = player;
-      if(validSearch("right", r, c, player, enemy))
-         board[r+1][c] = player;
-      if(validSearch("up", r, c, player, enemy))
-         board[r][c-1] = player;
-      if(validSearch("down", r, c, player, enemy))
-         board[r][c+1] = player;
-      if(validSearch("leftup", r, c, player, enemy))
-         board[r-1][c-1] = player;
-      if(validSearch("leftdown", r, c, player, enemy))
-         board[r-1][c+1] = player;
-      if(validSearch("rightup", r, c, player, enemy))
-         board[r+1][c-1] = player;
-      if(validSearch("rightdown", r, c, player, enemy))
-         board[r+1][c+1] = player;
+      Move lastPos; //last valid player pos to flip pieces to
+      lastPos = validSearch("down", r, c, player, enemy);
+      if(lastPos != null){
+         while(row != lastPos.row){
+            board[++row][col] = player;
+         }
+         row = r;
+      }
+
+      lastPos = validSearch("up", r, c, player, enemy);
+      if(lastPos != null){
+         while(row != lastPos.row){
+            board[--row][col] = player;
+         }
+         row = r;
+      }
+
+      lastPos = validSearch("left", r, c, player, enemy);
+      if(lastPos != null){
+         while(col != lastPos.col){
+            board[row][--col] = player;
+         }
+         col = c;
+      }
+
+      lastPos = validSearch("right", r, c, player, enemy);
+      if(lastPos != null){
+         while(col != lastPos.col){
+            board[row][++col] = player;
+         }
+         col = c;
+      }
+
+      lastPos = validSearch("leftup", r, c, player, enemy);
+      if(lastPos != null){
+         while(row != lastPos.row && col != lastPos.col){
+            board[--row][--col] = player;
+         }
+         row = r;
+         col = c;
+      }
+      lastPos = validSearch("rightup", r, c, player, enemy);
+      if(lastPos != null){
+         while(row != lastPos.row && col != lastPos.col){
+            board[--row][++col] = player;
+         }
+         row = r;
+         col = c;
+      }
+
+      lastPos = validSearch("leftdown", r, c, player, enemy);
+      if(lastPos != null){
+         while(row != lastPos.row && col != lastPos.col){
+            board[++row][--col] = player;
+         }
+         row = r;
+         col = c;
+      }
+
+      lastPos = validSearch("rightdown", r, c, player, enemy);
+      if(lastPos != null){
+         while(row != lastPos.row && col != lastPos.col){
+            board[++row][++col] = player;
+         }
+         row = r;
+         col = c;
+      }
+      
    }
 
   /* public static boolean flip(Move move, int player, int enemy, int row, int col){
@@ -253,44 +305,44 @@ public class Othello{
          for(int c = 0; c < size; c++){
             if(board[r][c] == blank){
 
-               boolean added = false;
+               Move added;
             	//search left
                added = validSearch("left", r, c, player, enemy);
 
             	//search right
-               if(!added)
+               if(added == null)
                   added = validSearch("right", r, c, player, enemy);
 
             	//search below
-               if(!added)
+               if(added == null)
                   added = validSearch("down", r, c, player, enemy);
 
             	//search above
-               if(!added)
+               if(added == null)
                   added = validSearch("up", r, c, player, enemy);
 
             	//search diagonal left up
-               if(!added)
+               if(added == null)
                   added = validSearch("leftup", r, c, player, enemy);
 
             	//search diagonal left down
-               if(!added)
+               if(added == null)
                   added = validSearch("leftdown", r, c, player, enemy);
 
             	//search diagonal right up
-               if(!added)
+               if(added == null)
                   added = validSearch("rightup", r, c, player, enemy);
 
             	//search diagonal right down
-               if(!added)
+               if(added == null)
                   added = validSearch("rightdown", r, c, player, enemy);
 
             }
          }
       }
    }
-
-   public static boolean validSearch(String dir, int r, int c, int player, int enemy){
+   //returns index of valid move extent. null for no valid move in that direction
+   public static Move validSearch(String dir, int r, int c, int player, int enemy){
    	//not end of board and the next space contains an enemy
       int orgR = r;
       int orgC = c;
@@ -299,148 +351,147 @@ public class Othello{
             c--;
 
             if(board[r][c] == blank)
-               return false;
+               return null;
             	//if friendly piece is encountered 
             else if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgC-c) == 1)
-                  return false;
+                  return null;
             		//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r,c+1));
             }
          }
-         return false;
+         return null;
       }
-      
+
       else if(dir.equals("right")){
          while(c < size-1){  
             c++;
 
             if (board[r][c] == blank)
-               return false;
+               return null;
             	//if friendly piece is encountered 
             else if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgC-c) == 1)
-                  return false;
+                  return null;
             		//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r,c-1));
             }
          }
-         return false;
+         return null;
       }
       else if(dir.equals("up")){
          while(r > 0){  
             r--;
-            
+
             if (board[r][c] == blank)
-               return false;
+               return null;
          		//if friendly piece is encountered 
             if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgR-r) == 1)
-                  return false;
+                  return null;
             		//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r+1,c));
             }
          }
-
-         return false;
+         return null;
       }
       else if(dir.equals("down")){
          while(r < size-1){  
             r++;
             if (board[r][c] == blank)
-               return false;
-         		//if friendly piece is encountered 
+               return null;
+         		//if friendly pieceis encountered 
             if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgR-r) == 1)
-                  return false;
+                  return null;
             		//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r-1,c));
             }
          }
-         return false;
+         return null;
       }
-      
+
       else if(dir.equals("leftup")){
          while(c > 0 && r > 0 ){  
             c--;
             r--;
             if (board[r][c] == blank)
-               return false;
+               return null;
          		//if friendly piece is encountered 
             if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgR-r) == 1 && Math.abs(orgC-c) == 1)
-                  return false;
+                  return null;
             			//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r+1,c+1));
             }
          }
-         return false;
+         return null;
       }
       else if(dir.equals("leftdown")){
          while(c > 0 && r > size-1){  
             c--;
             r++;
             if (board[r][c] == blank)
-               return false;
+               return null;
          		//if friendly piece is encountered 
             if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgR-r) == 1 && Math.abs(orgC-c) == 1)
-                  return false;
+                  return null;
             			//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r-1,c+1));
             }
          }
-         return false;
+         return null;
       }
       else if(dir.equals("rightup")){
          while(c < size-1 && r > 0 ){  
             c++;
             r--;
             if (board[r][c] == blank)
-               return false;
+               return null;
          		//if friendly piece is encountered 
             if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgR-r) == 1 && Math.abs(orgC-c) == 1)
-                  return false;
+                  return null;
             			//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r+1,c-1));
             }
          }
-         return false;
+         return null;
       }
       else if(dir.equals("rightdown")){
          while(c < size-1 && r > size-1 ){  
             c++;
             r++;
             if (board[r][c] == blank)
-               return false;
+               return null;
          		//if friendly piece is encountered 
             if(board[r][c] == player){
                //see if adjacent move is player
                if(Math.abs(orgR-r) == 1 && Math.abs(orgC-c) == 1)
-                  return false;
+                  return null;
             			//add to list of valid moves (and directions?)
                legalMoves.add(new Move(orgR,orgC));
-               return true;
+               return (new Move(r-1,c-1));
             }
+            return null;
          }
-         return false;
       }
-      return false;
+      return null;
    }
 
    public static class Move{
